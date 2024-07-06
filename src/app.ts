65c79618443691971d761d3c7bef983db42353ba
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2024-07-07 00:32:24 +0300

    add colors to logs

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { getExamRouter } from "./routes/exam";
 import { ticketEditorRouter } from "./routes/ticketEditor";
 import { userEditorRouter } from "./routes/userEditor";
 import { questionRouter } from "./routes/question";
+import { logRoutes } from "./assets/logRoutes";
 
 export const app = express();
 
@@ -14,23 +15,7 @@ app.use(cors());
 
 app.use(express.json({}));
 
-app.use((req, res, next) => {
-	const path = req.path;
-	const now = new Date();
-
-	const year = now.getFullYear();
-	const month = `0${now.getMonth() + 1}`.slice(-2);
-	const day = `0${now.getDate()}`.slice(-2);
-	const hours = `0${now.getHours()}`.slice(-2);
-	const minutes = `0${now.getMinutes()}`.slice(-2);
-
-	if (path.includes("swagger")) return next();
-	console.log(
-		`Запрос на роут ${path} с методом ${req.method} ${day}/${month}/${year} в ${hours}:${minutes}`,
-	);
-
-	next();
-});
+app.use(logRoutes);
 
 app.use("/auth", getAuthRouter());
 app.use("/tickets", getTicketsRouter());
--- src/assets/logStyles.ts
+++ src/assets/logStyles.ts
@@ -4,6 +4,7 @@ export const colors = {
 	green: "\x1b[32m",
 	yellow: "\x1b[33m",
 	blue: "\x1b[34m",
+	cyan: "\x1b[46m",
 	whiteblue: "\x1b[36m",
 	white: "\x1b[37m",
 };
