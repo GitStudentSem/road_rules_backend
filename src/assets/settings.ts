e5e663a1eef18f90536a641d6a65c4a01d53dcda
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2024-08-03 12:32:18 +0300

    fix default settings

--- src/assets/settings.ts
+++ src/assets/settings.ts
@@ -6,9 +6,9 @@ export const settings = {
 export const defaultSwaggerValues = {
 	authToken:
 		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxNzIwMjk4MzgyNTUyIiwiaWF0IjoxNzIwMzI3NzA3LCJleHAiOjE3MjI5MTk3MDd9.QgIn3VTLVckneB1EqROzpGTBm58iQhhYMd205g3F7Ag",
-	ticketId: "1719675339512",
-	questionId: "1719675555920",
-	answerId: "17199090369010",
+	ticketId: "1722254296932",
+	questionId: "1722674857532",
+	answerId: "17226748575851",
 	email: "your_email@yandex.ru",
 	password: "123456",
 };
