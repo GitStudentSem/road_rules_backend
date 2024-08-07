620931ad53fe826a75e40d62320a8e7298ec7c0c
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2024-08-07 14:21:48 +0300

    fix validations

--- src/validations.ts
+++ src/validations.ts
@@ -69,17 +69,28 @@ export const registerValidation = [
 export const answerValidation = [
 	checkNullable("ticketId"),
 	body("ticketId", "id билета должен быть строкой").isString(),
+
 	body("questionId", "id вопроса должен быть строкой").isString(),
+
 	body("answerId", "id ответа пользователя должен быть строкой").isString(),
 ];
 
 export const addQuestionValidation = [
 	body("ticketId", "id билета должно быть строкой").isString(),
 	body("ticketId", "id не может быть пустым").trim().isLength({ min: 1 }),
-	body("question", "Минимальное число сивмолов для попроса: 10").isLength({
+
+	body("question", "Минимальное число сивмолов для вопроса: 1")
+		.trim()
+		.isLength({
+			min: 1,
+		}),
+
+	body("help", "Помощь по вопросу должна быть строкой").isString(),
+	body("help", "Минимальное число сивмолов для подсказки: 1").trim().isLength({
 		min: 1,
 	}),
-	body("help", "Минимальное число сивмолов для подсказки: 10").isString(),
+
+	body("correctAnswer", "Не выбран правильный ответ").exists(),
 	body("correctAnswer", "Правильный ответ должен быть числом").isNumeric(),
 
 	body("answers", "Варианты ответов должны быть массивом").isArray(),
