c5fa57234bb16ff57be5c880b0c4ebd66e943a69
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2024-08-22 13:56:25 +0300

    fix not found referer

--- src/assets/checkURLAccess.ts
+++ src/assets/checkURLAccess.ts
@@ -4,35 +4,33 @@ import { HTTP_STATUSES } from "../utils";
 const tgLink = "(telegram: @semyon_purnemtzev)";
 //activeUntill: year, month, date,
 const serverWillDeadAt = new Date(2024, 9, 0);
-const allowedReferers = [
-	{ url: "http://localhost", activeUntill: serverWillDeadAt },
-];
+const allowedReferers = [{ url: "localhost", activeUntill: serverWillDeadAt }];
 
 export const checkURLAccess = (
 	req: Request,
 	res: Response,
 	next: NextFunction,
 ) => {
-	const referer = req.get("Referer");
-
-	if (!referer) {
+	const host = req.get("Host");
+	console.log("host", host);
+	if (!host) {
 		res
 			.status(HTTP_STATUSES.FORRIBDEN_403)
 			.send(
-				`У вас отсутсвует заголовок referer, он необходим для выполнения запроса, попробуйте обратиться к администратору ${tgLink}`,
+				`У вас отсутсвует заголовок host, он необходим для выполнения запроса, попробуйте обратиться к администратору ${tgLink}`,
 			);
 		return;
 	}
 
 	const refererEntry = allowedReferers.find((entry) =>
-		referer.startsWith(entry.url),
+		host.startsWith(entry.url),
 	);
 
 	if (!refererEntry) {
 		res
 			.status(HTTP_STATUSES.FORRIBDEN_403)
 			.send(
-				`Ваш адрес: ${referer} не включен в список разрешенных адресов этого сервера, пожалуйста обратитесь к администратору ${tgLink}`,
+				`Ваш адрес: ${host} не включен в список разрешенных адресов этого сервера, пожалуйста обратитесь к администратору ${tgLink}`,
 			);
 		return;
 	}
@@ -52,7 +50,7 @@ export const checkURLAccess = (
 		res
 			.status(HTTP_STATUSES.FORRIBDEN_403)
 			.send(
-				`Срок оплаты сервера для этого адреса: ${referer} истек ${formattedDate}, вы можете оплатить его снова и вернуть себе доступ, попробуйте обратиться к администратору ${tgLink}`,
+				`Срок оплаты сервера для этого адреса: ${host} истек ${formattedDate}, вы можете оплатить его снова и вернуть себе доступ, попробуйте обратиться к администратору ${tgLink}`,
 			);
 		return;
 	}
